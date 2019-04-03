1759b8cfabe1a51d25aa99fe35b8b75569943ee8
libra189 <libra189@gmail.com>
2019-04-03 23:34:06 +0900

    feature element-ui追加

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <el-row>
+        <el-col :span="6"><div class="grid-content bg-purple">Home</div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light">About</div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple">Works</div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light">Skil</div></el-col>
+    </el-row>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Element)
+
 new Vue({
     router,
+    el: '#app',
     render: h => h(App)
 }).$mount('#app')

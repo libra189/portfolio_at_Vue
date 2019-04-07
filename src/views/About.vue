27d4b7f471fe39a14c7760897523a0604de5ae1e
libra189 <libra189@gmail.com>
2019-04-07 23:24:15 +0900

    fix 経歴のスタイルを修正

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,15 +5,15 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-lg-offset-2">
-
             <div class="section-heading text-center">
+              <small class="text-warning">About</small>
               <div class="wow bounceInDown" data-wow-delay="0.2s">
                 <h2>山下大輔</h2>
               </div>
-              <strong>将来の目標</strong>
-              <p class="wow bounceInUp" data-wow-delay="0.3s">
-                他のエンジニアより三歩リードしたエンジニアになる
-              </p>
+              <div class="goal">
+                <span>将来の目標</span>
+                <p class="lead"><strong>他のエンジニアより三歩リードしたエンジニアになる</strong></p>
+              </div>
             </div>
           </div>
         </div>
@@ -26,14 +26,32 @@
           </div>
           <div class="col-md-7">
             <p><strong>経歴</strong></p>
-            <ul class="text-left">
-              <li>2012.3 龍谷大学 理工学部 電子情報学科 卒業</li>
-              <li>2012.4 龍谷大学 大学院 理工学研究科 電子情報学専攻 入学</li>
-              <li>2015.4 龍谷大学 大学院 理工学研究科 電子情報学専攻 卒業</li>
-              <li>2015.11 Jetrunテクノロジ株式会社 入社</li>
-              <li>2016.11 Jetrunテクノロジ株式会社 退社</li>
-              <li>2016.12 インフォニック株式会社 入社</li>
-            </ul>
+            <table id="career">
+              <tr>
+                <td>2012.3</td>
+                <td>龍谷大学 理工学部 電子情報学科 卒業</td>
+              </tr>
+              <tr>
+                <td>2012.4</td>
+                <td>龍谷大学 大学院 理工学研究科 電子情報学専攻 入学</td>
+              </tr>
+              <tr>
+                <td>2015.4</td>
+                <td>龍谷大学 大学院 理工学研究科 電子情報学専攻 卒業</td>
+              </tr>
+              <tr>
+                <td>2015.11</td>
+                <td>Jetrunテクノロジ株式会社 入社</td>
+              </tr>
+              <tr>
+                <td>2016.11</td>
+                <td>Jetrunテクノロジ株式会社 退社</td>
+              </tr>
+              <tr>
+                <td>2016.12</td>
+                <td>インフォニック株式会社 入社</td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
@@ -45,5 +63,18 @@
 <style scoped>
 ul {
   list-style: none;
+}
+#career {
+  border: 0px #000000 solid;
+  border-collapse: separate;
+  border-spacing: 5px;
+  margin-left: auto;
+  margin-right: auto;
+}
+#career td {
+  text-align: left
+}
+.goal {
+  margin-top: 2rem;
 }
 </style>

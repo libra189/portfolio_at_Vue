99b87404fdfe55544848abad4e36934410e30e5f
libra189 <libra189@gmail.com>
2019-04-07 19:43:27 +0900

    add 経歴追加

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,49 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- Section: about -->
+    <section id="about" class="home-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-lg-offset-2">
+
+            <div class="section-heading text-center">
+              <div class="wow bounceInDown" data-wow-delay="0.2s">
+                <h2>山下大輔</h2>
+              </div>
+              <strong>将来の目標</strong>
+              <p class="wow bounceInUp" data-wow-delay="0.3s">
+                他のエンジニアより三歩リードしたエンジニアになる
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5">
+            <img src="img/img1.jpg" class="img-responsive img-rounded" alt="" />
+          </div>
+          <div class="col-md-7">
+            <p><strong>経歴</strong></p>
+            <ul class="text-left">
+              <li>2012.3 龍谷大学 理工学部 電子情報学科 卒業</li>
+              <li>2012.4 龍谷大学 大学院 理工学研究科 電子情報学専攻 入学</li>
+              <li>2015.4 龍谷大学 大学院 理工学研究科 電子情報学専攻 卒業</li>
+              <li>2015.11 Jetrunテクノロジ株式会社 入社</li>
+              <li>2016.11 Jetrunテクノロジ株式会社 退社</li>
+              <li>2016.12 インフォニック株式会社 入社</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /Section: about -->
   </div>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+</style>

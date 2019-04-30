f1e8d5fda6f9b1c30a4f7b7b41b2066927053af9
libra189 <libra189@gmail.com>
2019-05-01 01:28:06 +0900

    refact コーディングスタイルの修正

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,16 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    mode="horizontal"
-    router
-    >
-    <el-menu-item index="home" :route="{ name:'home' }" style="height: 100%">Home</el-menu-item>
-    <el-menu-item index="about" :route="{ name:'about' }" style="height: 100%">About</el-menu-item>
-    <el-menu-item index="works" :route="{ name:'works' }" style="height: 100%">Works</el-menu-item>
-    <el-menu-item index="skil" :route="{ name:'skil' }" style="height: 100%">Skil</el-menu-item>
-  </el-menu>
+  <div id="header">
+    <el-menu
+      :default-active="activeIndex"
+      mode="horizontal"
+      router
+      >
+        <el-menu-item index="home" :route="{ name:'home' }" style="height: 100%">Home</el-menu-item>
+        <el-menu-item index="about" :route="{ name:'about' }" style="height: 100%">About</el-menu-item>
+        <el-menu-item index="works" :route="{ name:'works' }" style="height: 100%">Works</el-menu-item>
+        <el-menu-item index="skil" :route="{ name:'skil' }" style="height: 100%">Skil</el-menu-item>
+    </el-menu>
+  </div>
 </template>
 
 <style scoped>
@@ -19,14 +21,14 @@
 
 <script>
 export default {
-  data() {
-      return {
-          activeIndex: 'home'
-      }
-  },
-  mounted() {
-      // 読み込まれたページをアクティブ
-      this.activeIndex = this.$route.name;
-  }
+    data () {
+        return {
+            activeIndex: 'home'
+        }
+    },
+    mounted () {
+        // 読み込まれたページをアクティブ
+        this.activeIndex = this.$route.name
+    }
 }
 </script>
